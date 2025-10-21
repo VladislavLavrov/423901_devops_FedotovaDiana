f382a305a4f0d0ec714203fb4ae3bcef08f9fd18
Diana <Diana.fedotova2002@mail.ru>
2025-10-21 12:49:15 +0500

    21.10.2025
    Добавление приложения калькулятора

---
+++ App_practical/Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // Отображение страницы
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Серверный метод для вычислений (AJAX)
+        [HttpPost]
+        public JsonResult Calculate(double num1, double num2, string operation)
+        {
+            double result = 0;
+            string message = "";
+
+            switch (operation)
+            {
+                case "+": result = num1 + num2; break;
+                case "-": result = num1 - num2; break;
+                case "*": result = num1 * num2; break;
+                case "/":
+                    if (num2 == 0)
+                        message = "Ошибка: деление на ноль!";
+                    else
+                        result = num1 / num2;
+                    break;
+                default:
+                    message = "Неизвестная операция!";
+                    break;
+            }
+
+            return Json(new { result, message });
+        }
+    }
+}
+
+
