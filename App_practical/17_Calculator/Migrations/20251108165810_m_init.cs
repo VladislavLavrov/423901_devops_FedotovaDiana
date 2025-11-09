c0a29d10df82d8906b5a6400458ff9b449b06b0b
Diana <Diana.fedotova2002@mail.ru>
2025-11-09 12:24:41 +0500

    09.11.2025 1
    правки в коде

---
+++ App_practical/17_Calculator/Migrations/20251108165810_m_init.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _17_Calculator.Migrations
+{
+    /// <inheritdoc />
+    public partial class m_init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "DataInputVariants",
+                columns: table => new
+                {
+                    ID_DataInputVariant = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Operand_1 = table.Column<double>(type: "double", nullable: false),
+                    Operand_2 = table.Column<double>(type: "double", nullable: false),
+                    Type_operation = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Result = table.Column<double>(type: "double", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DataInputVariants", x => x.ID_DataInputVariant);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DataInputVariants");
+        }
+    }
+}
--- App_practical/17_Calculator/Models/DataInputVariant.cs
+++ App_practical/17_Calculator/Models/DataInputVariant.cs
@@ -1,12 +1,14 @@
-﻿namespace Calculator.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Calculator.Models
 {
-    // Модель данных для хранения операций калькулятора
     public class DataInputVariant
     {
-        public int ID_DataInputVariant { get; set; }   // Первичный ключ
-        public double Operand_1 { get; set; }          // Первый операнд
-        public double Operand_2 { get; set; }          // Второй операнд
-        public string Type_operation { get; set; } = string.Empty;  // Тип операции (+, -, *, /)
-        public double Result { get; set; }             // Результат вычисления
+        [Key]
+        public int ID_DataInputVariant { get; set; }
+        public double Operand_1 { get; set; }
+        public double Operand_2 { get; set; }
+        public string Type_operation { get; set; } = string.Empty;
+        public double Result { get; set; }
     }
 }
